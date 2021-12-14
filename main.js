cb79388b5b67803845a252882f576527bfe434d6
AkshayaAkshaj <87810890+AkshayaAkshaj@users.noreply.github.com>
2021-12-14 16:46:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-function preload(){
+noseY=0;
+nosex=0;
 
+function preload(){
+lips=loadImage("https://i.postimg.cc/hPNF8mqn/l1.png");
 }
 function setup(){
 canvas=createCanvas(300,300);
@@ -17,6 +20,7 @@ console.log('PoseNet is Initialized');
 }
 function draw(){
 image(video , 0 , 0 , 300 ,300 );
+image(lips,nosex,noseY,50,30);
 }
 
 function take_snapshot(){
@@ -24,6 +28,8 @@ function take_snapshot(){
 }
 function gotPoses(results){
     if(results.length > 0) {
+        noseY=results[0].pose.nose.y+30;
+        nosex=results[0].pose.nose.x-25;
         console.log(results);
         console.log("nose x = "+ results[0].pose.nose.x);
         console.log("nose y = "+ results[0].pose.nose.y);
